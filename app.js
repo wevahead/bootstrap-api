d7d7753a2647200a98bfa3276ce096c8eca45f34
khassenkamp <khassenkamp@gmail.com>
2019-12-19 22:35:09 -0600

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-document.getElementsByClassName('nav-item')
+// document.getElementsByClassName('nav-item')
 
-window.addEventListener('hashchange', console.log('wowza'))
+const setTab = (hash)=> {
+    const active = document.querySelector('a.active');
+    if(active) {
+        active.classList.remove('active');
+    }
+    const link = document.querySelector(`a[hrefs='${hash}']`);
+};
+
+window.addEventListener('hashchange', ()=> {
+    setTab(window.location.hash);
+})
+
+if(!window.location.hash) {
+    window.location.hash = '#products';
+} else {
+    setTab(window.location.hash);
+}
+
+const getData = async (hash)=> {
+    return axios.get(`https://acme-users-api-rev.herokuapp.com/api/${hash}`)
+    .then( response => response.data)
+    .then( products => console.log(products))
+}
+console.log(getData)
